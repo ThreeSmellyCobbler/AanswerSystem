3635d14304643f0f51a03cfd047299deffec2a55
chenjia <chenjia02@@chinatopcredit.com>
2018-11-29 20:57:52 +0800

    补全注释

--- member/src/main/java/com/tsco/member/MemberApplication.java
+++ member/src/main/java/com/tsco/member/MemberApplication.java
@@ -3,11 +3,16 @@ package com.tsco.member;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
+
+/**
+ * @author chenjia
+ * member 项目启动类
+ */
 @SpringBootApplication
-public class memberApplication {
+public class MemberApplication {
 
     public static void main(String[] args) {
-        new SpringApplicationBuilder(memberApplication.class).run(args);
+        new SpringApplicationBuilder(MemberApplication.class).run(args);
     }
 
 }
