2c4b5ea4aefbf02420feb09b9208b298af4443a3
chenjia <chenjia02@@chinatopcredit.com>
2018-12-02 10:23:46 +0800

    配置登录拦截器

---
+++ web/src/main/java/com/tsco/web/config/LoginInterceptor.java
@@ -0,0 +1,37 @@
+package com.tsco.web.config;
+
+import com.tsco.web.utils.Constans;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 登录拦截器
+ *
+ * @author chen jia
+ */
+
+@Component
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (request.getSession().getAttribute(Constans.USER_ID) == null) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
+
+    }
+}
---
+++ web/src/main/java/com/tsco/web/config/WebConfig.java
@@ -0,0 +1,42 @@
+package com.tsco.web.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.Arrays;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Autowired
+    private LoginInterceptor loginInterceptor;
+
+    /**
+     * 拦截器配置
+     *
+     * @param registry
+     * @auto chen jia
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(loginInterceptor)
+                //拦截所有请求
+                .addPathPatterns("/**")
+                //排除登录、注册
+                .excludePathPatterns(Arrays.asList("/login", "/register"));
+    }
+
+    /**
+     * 静态资源处理
+     *
+     * @param registry
+     */
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
+}
