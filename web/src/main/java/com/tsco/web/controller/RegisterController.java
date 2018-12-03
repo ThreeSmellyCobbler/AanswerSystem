fe0f28264ca4f5993fe8d1b49b1bbc3fa5ee693f
chenjia <chenjia02@@chinatopcredit.com>
2018-12-03 20:45:42 +0800

    fix

--- web/src/main/java/com/tsco/web/controller/RegisterController.java
+++ web/src/main/java/com/tsco/web/controller/RegisterController.java
@@ -1,13 +1,14 @@
 package com.tsco.web.controller;
 
 import com.tsco.api.domain.UserDTO;
-import com.tsco.web.domain.vo.RegisterForm;
 import com.tsco.web.domain.Response;
+import com.tsco.web.domain.vo.RegisterForm;
 import com.tsco.web.exception.ExceptionCode;
 import com.tsco.web.exception.WebException;
 import com.tsco.web.service.impl.UserService;
 import com.tsco.web.utils.PatternRegex;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,7 +22,7 @@ public class RegisterController {
     @Autowired
     private UserService userService;
 
-    @RequestMapping(method = RequestMethod.POST)
+    @RequestMapping(method = RequestMethod.POST,consumes = MediaType.APPLICATION_JSON_VALUE,produces = MediaType.APPLICATION_JSON_VALUE)
     public Response register(RegisterForm registerForm) {
         if (registerForm.getEmail() == null || registerForm.getPassword() == null) {
             throw new WebException(ExceptionCode.INVALID_PARAMETER, "邮箱或者密码不能为空");
