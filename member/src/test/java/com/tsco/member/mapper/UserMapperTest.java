21e50de94fb8dc9b036a99aa06d858387348e605
chenjia <chenjia02@@chinatopcredit.com>
2019-01-08 13:22:01 +0800

    修改端口号

--- member/src/test/java/com/tsco/member/mapper/UserMapperTest.java
+++ member/src/test/java/com/tsco/member/mapper/UserMapperTest.java
@@ -1,14 +1,11 @@
 package com.tsco.member.mapper;
 
-import com.tsco.member.domain.po.User;
 import com.tsco.member.mapper.user.UserMapper;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import java.util.Date;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest
@@ -23,46 +20,6 @@ public class UserMapperTest {
         System.out.println(userMapper);
         System.out.println("====================");
 
-    }
-
-
-    @Test
-    public void testLoad() {
-        userMapper.load(1L);
-    }
-
-    @Test
-    public void testInsert() {
-        User user = User.builder()
-                .userName("1")
-                .userRole("1")
-                .address("1")
-                .email("1")
-                .birthday(new Date())
-                .password("1")
-                .gender("1")
-                .build();
-        System.out.println(userMapper.persit(user));
-    }
-
-    @Test
-    public void testUpdate() {
-        User user = User.builder()
-                .id(7L)
-                .userName("2")
-                .userRole("2")
-                .address("2")
-                .email("2")
-                .birthday(new Date())
-                .password("3")
-                .gender("4")
-                .build();
-        System.out.println(userMapper.update(user));
-    }
-
-    @Test
-    public void testFindByEmail() {
-        userMapper.findByEmail("123qq@qq.com");
     }
 
 }
