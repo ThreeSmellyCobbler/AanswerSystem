7e073bec205e967a23ecd0200555d1eaa3cd090a
chenjia <chenjia02@@chinatopcredit.com>
2019-01-08 12:53:15 +0800

    获取验证码接口

---
+++ api/src/main/java/com/tsco/api/domain/dto/UserDTO.java
@@ -0,0 +1,29 @@
+package com.tsco.api.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+import java.util.Date;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO implements Serializable{
+
+    private Long id;
+    private String userName;
+    private String email;
+    private String password;
+    private String gender;
+    private Date birthday;
+    private String address;
+    private String userRole;
+    private Date createdAt;
+    private Date updatedAt;
+
+}
+
