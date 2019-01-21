409f3b0de16fb78128cdfba8b6fcc2830b5a1833
chenjia <chenjia02@@chinatopcredit.com>
2019-01-21 11:15:57 +0800

    测试不再连接服务器，统一连接本地应用

--- answer/src/test/java/com/tsco/answer/Mapper/AnswerRecordMapperTest.java
+++ answer/src/test/java/com/tsco/answer/Mapper/AnswerRecordMapperTest.java
@@ -1,8 +1,6 @@
 package com.tsco.answer.Mapper;
 
-import com.tsco.answer.domain.po.AnswerRecord;
 import com.tsco.answer.mapper.AnswerRecordMapper;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
