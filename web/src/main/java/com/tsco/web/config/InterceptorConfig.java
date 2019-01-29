befc33df0aafd1d73c8d6d202d2a6cc200d7e72d
chenjia <chenjia02@@chinatopcredit.com>
2019-01-29 15:55:07 +0800

    fix-跨域拦截器

--- web/src/main/java/com/tsco/web/config/InterceptorConfig.java
+++ web/src/main/java/com/tsco/web/config/InterceptorConfig.java
@@ -4,6 +4,7 @@ import com.tsco.web.config.Interceptor.CrossDomainInterceptor;
 import com.tsco.web.config.Interceptor.WebInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -23,8 +24,8 @@ public class InterceptorConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         //这里可以添加多个拦截器,组成一个拦截器链
-        registry.addInterceptor(crossDomainInterceptor)
-                .addPathPatterns("/**");
+        /*registry.addInterceptor(crossDomainInterceptor)
+                .addPathPatterns("/**");*/
         registry.addInterceptor(webInterceptor)
                 //拦截所有请求
                 .addPathPatterns("/**");
@@ -42,5 +43,14 @@ public class InterceptorConfig implements WebMvcConfigurer {
                 .addResourceLocations("classpath:/META-INF/resources/");
         registry.addResourceHandler("/webjars/**")
                 .addResourceLocations("classpath:/META-INF/resources/webjars/");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("POST", "GET", "PUT", "OPTIONS", "DELETE")
+                .maxAge(3600)
+                .allowCredentials(true);
     }
 }
