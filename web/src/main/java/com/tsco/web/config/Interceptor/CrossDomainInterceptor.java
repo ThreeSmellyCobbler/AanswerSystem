f43f7b090cb3e167782f0398976a3e8e0574b106
chenjia <chenjia02@@chinatopcredit.com>
2019-01-29 15:39:18 +0800

    fix-跨域拦截器

--- web/src/main/java/com/tsco/web/config/Interceptor/CrossDomainInterceptor.java
+++ web/src/main/java/com/tsco/web/config/Interceptor/CrossDomainInterceptor.java
@@ -16,8 +16,9 @@ public class CrossDomainInterceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Headers", "X-Requested-With,content-type,token");
-        response.setHeader("Access-Control-Allow-Methods", "GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.addHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         return true;
     }
 
