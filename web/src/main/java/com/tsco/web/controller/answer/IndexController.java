76c81b43226796ecd3d1dd3e7ea96825a87a48a5
chenjia <chenjia02@@chinatopcredit.com>
2019-02-15 17:46:19 +0800

    fix-Invalid bound statement

--- web/src/main/java/com/tsco/web/controller/answer/IndexController.java
+++ web/src/main/java/com/tsco/web/controller/answer/IndexController.java
@@ -11,12 +11,12 @@ import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.Resource;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,7 +27,7 @@ import java.util.stream.Collectors;
 @RequestMapping("/index")
 public class IndexController {
 
-    @Autowired
+    @Resource(name = "answerServiceImpl")
     private AnswerService answerService;
 
     @ApiOperation(value = "查询题目列表", notes = "get-subject-list")
